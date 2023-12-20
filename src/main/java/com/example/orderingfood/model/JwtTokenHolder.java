fd765f5281339a8df1d114a773fc65da27f8b2a6
Bartek Boćkowski <bboc12357@gmail.com>
2023-12-20 13:24:06 +0100

    fix

--- src/main/java/com/example/orderingfood/model/JwtTokenHolder.java
+++ src/main/java/com/example/orderingfood/model/JwtTokenHolder.java
@@ -1,8 +1,10 @@
 package com.example.orderingfood.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class JwtTokenHolder {
 
     private String token;
