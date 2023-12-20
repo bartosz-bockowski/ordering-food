7342adef80de4da70fdaa6c698037c6b1be40e43
Bartek Boćkowski <bboc12357@gmail.com>
2023-12-20 14:02:49 +0100

    global exception handler - validate

--- src/main/java/com/example/orderingfood/config/GlobalExceptionHandler.java
+++ src/main/java/com/example/orderingfood/config/GlobalExceptionHandler.java
@@ -5,8 +5,14 @@ import com.example.orderingfood.exception.WrongLoginCredentialsException;
 import com.example.orderingfood.model.ApiError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -19,6 +25,23 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ApiError> handleNotFoundException(NotFoundException e) {
         return new ResponseEntity<>(new ApiError(e.getMessage(), HttpStatus.NOT_FOUND), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, List<String>>> handleValidationErrors(MethodArgumentNotValidException e) {
+        List<String> errors = e.getBindingResult().getFieldErrors().stream()
+                .map((error) ->
+                        error.getField()
+                                .concat(": ")
+                                .concat(Optional.ofNullable(error.getDefaultMessage()).orElse("")))
+                .toList();
+        return new ResponseEntity<>(getErrorsMap(errors), HttpStatus.BAD_REQUEST);
+    }
+
+    private Map<String, List<String>> getErrorsMap(List<String> errors) {
+        Map<String, List<String>> errorResponse = new HashMap<>();
+        errorResponse.put("errors", errors);
+        return errorResponse;
     }
 
 }
